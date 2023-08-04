a2be087d21bd616f5e5e13709b5fd938cbee04a0
Thanhduy_Hachi-x <duy.nguyen@hachi-x.com>
2023-08-04 17:53:51 +0700

    :1234: stop at scroll animate

---
+++ animateScroll/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  console.log('start scroll learning');
+  let page_url = window.location.href;
+  let page_id = page_url.substring(page_url.lastIndexOf('#') + 1);
+});
