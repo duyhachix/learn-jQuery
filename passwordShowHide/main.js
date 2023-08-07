d5dabbeffa1f643b79a9a60de12d72346a9f46b0
Thanhduy_Hachi-x <duy.nguyen@hachi-x.com>
2023-08-07 14:58:47 +0700

    update formaat

--- passwordShowHide/main.js
+++ passwordShowHide/main.js
@@ -1,14 +1,14 @@
 let viewLoginPwd = false;
 
 function changePwdType() {
-    let getPwdType = $('#viewPwdLogin');
+  let getPwdType = $('#viewPwdLogin');
 
-    if (viewLoginPwd === false) {
-        console.log();
-        getPwdType.attr('type', 'text');
-        viewLoginPwd = true;
-    } else if (viewLoginPwd === true) {
-        getPwdType.attr('type', 'password');
-        viewLoginPwd = false;
-    }
+  if (viewLoginPwd === false) {
+    console.log();
+    getPwdType.attr('type', 'text');
+    viewLoginPwd = true;
+  } else if (viewLoginPwd === true) {
+    getPwdType.attr('type', 'password');
+    viewLoginPwd = false;
+  }
 }
