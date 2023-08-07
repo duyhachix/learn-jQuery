bb69c69ffa3010ae301c1242ac882972c175446b
Thanhduy_Hachi-x <duy.nguyen@hachi-x.com>
2023-08-07 14:30:41 +0700

    :scroll:  scroll

--- animateScroll/main.js
+++ animateScroll/main.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
-  console.log('start scroll learning');
   let page_url = window.location.href;
+  console.log(page_url);
   let page_id = page_url.substring(page_url.lastIndexOf('#') + 1);
+  if (page_id == 'section3') {
+    $('html, body').animate(
+      {
+        scrollTop: $('#scroll-' + page_id).offset().top,
+      },
+      1000
+    );
+  } else if (page_id == 'post') {
+    $('html, body').animate(
+      {
+        scrollTop: $('#scroll-' + 'post').offset().top,
+      },
+      1000
+    );
+  }
 });
